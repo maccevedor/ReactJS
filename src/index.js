a7eadeddd1cb1fbc9958b18270a1c5abab19eb35
Mauricio Acevedo Rueda <maccevedor@gmail.com>
2018-02-10 17:49:36 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const rootElement = document.getElementById("root");
+const element = document.createElement("div");
+element.textContent = "hello world";
+element.className = "container";
+rootElement.appendChild(element);
